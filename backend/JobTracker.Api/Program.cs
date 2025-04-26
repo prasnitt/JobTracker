4e66653f07183eb6743aab750267517871c515d5
Prashant Agrawal <prashant.agrawal@2degrees.nz>
2025-04-27 11:10:33 +1200

    add healthy endpoint

--- backend/JobTracker.Api/Program.cs
+++ backend/JobTracker.Api/Program.cs
@@ -23,6 +23,8 @@ builder.Services.AddCors(options =>
     });
 });
 
+builder.Services.AddHealthChecks();
+
 // Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
@@ -41,7 +43,7 @@ app.UseCors("ConfiguredCors");
 app.UseSwagger();
 app.UseSwaggerUI();
 
-
+app.UseHealthChecks("/healthy");
 app.UseAuthorization();
 app.MapControllers();
 
